e2be54d8e24c2b60bcecc416a0cd91072004e6d2
MEET1733 <meetharsoda5@gmail.com>
2024-12-31 03:19:34 +0530

    Updated-Routes-and-MongoDB-models

--- user/controllers/user.controller.js
+++ user/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const userModel = require('../models/user.model')
+const blacklisttokenModel = require('../models/blacklisttoken.model');
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
@@ -18,8 +19,56 @@ module.exports.register = async (req, res) => {
         const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' })
 
         res.cookie('token', token);
-        res.send({ message: 'User registered succeddfully' })
+        res.send({ message: 'User registered successfully' })
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 };
+
+module.exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userModel
+            .findOne({ email })
+            .select('+password');
+
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.cookie('token', token);
+        res.send({ message: 'User logged in successfully' })
+
+    } catch (error) {
+
+        res.status(500).json({ message: error.message });
+    }
+
+}
+
+module.exports.logout = async (req, res) => {
+    try {
+        const token = req.cookies.token;
+        await blacklisttokenModel.create({ token });
+        res.clearCookie('token');
+        res.send({ message: 'User logged out successfully' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+module.exports.profile = async (req, res) => {
+    try {
+        res.send(req.user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
