9d9e027dbc90ca62693b8317a8f8d6b87e316cc3
MEET1733 <meetharsoda5@gmail.com>
2025-01-01 02:06:46 +0530

    Added-Ride-Service

---
+++ ride/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const rideRoutes = require('./routes/ride.routes')
+const dotenv = require('dotenv')
+dotenv.config()
+const cookieParser = require('cookie-parser')
+const connect = require('./db/db')
+connect()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+app.use('/', rideRoutes);
+
+module.exports = app
